8f9f60a71a800020d5ad1f3b87657516ff655008
joborbits <joborbits@gmail.com>
2018-03-18 21:18:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,13 @@ echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
+echo -e -n "$addProtocol" >> config.json
+echo -e -n "$CONFIG_JSON4" >> config.json
+echo -e -n "$addProtocolPort" >> config.json
+echo -e -n "$CONFIG_JSON5" >> config.json
+echo -e -n "$addProtocolmethod" >> config.json
+echo -e -n "$CONFIG_JSON6" >> config.json
+echo -e -n "$addProtocolPass" >> config.json
+echo -e -n "$CONFIG_JSON7" >> config.json
 
 ./v2ray
